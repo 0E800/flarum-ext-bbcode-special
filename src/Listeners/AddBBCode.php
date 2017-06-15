aa576c04c3077a10fb52e13aa65bf510f76365ba
0E800 <0ld3nglish800@gmail.com>
2017-06-15 23:08:21 +0000

    modified:   less/extension.less 	modified:   src/Listeners/AddBBCode.php

--- src/Listeners/AddBBCode.php
+++ src/Listeners/AddBBCode.php
@@ -60,14 +60,70 @@ class AddBBCode
         );
 
 $event->configurator->BBCodes->addCustom(
-            '[ELECTRIFY]{TEXT},{TEXT2},{TEXT3},{TEXT4}[/ELECTRIFY]',
-'<svg xmlns:xlink="www.w3.org/1999/xlink" width="100%" height="100%"><defs><text id="{TEXT}" x="50%" y="50%" font-family="Voltaire" font-size="68px" font-weight="600" text-anchor="middle" alignment-baseline="middle">{TEXT}</text></defs><filter id="glow"> <feGaussianBlur id="glowBlur" stdDeviation="3.5" result="coloredBlur"/><feMerge><feMergeNode in="coloredBlur"/><feMergeNode in="SourceGraphic"/></feMerge></filter><filter id="shock"><feTurbulence type="fractalNoise" baseFrequency="0.2" numOctaves="1" seed="2" result="noise" id="noise"> <animate attributeType="XML" attributeName="seed" from="2" to="120" dur="12s" repeatCount="indefinite"/></feTurbulence><feMorphology id="morph1" in="SourceGraphic" operator="dilate" radius="10" result="morph1" /><feMorphology id="morph2" in="morph1" operator="dilate" radius="2" result="morph2" /><feComposite operator="out" in="morph2" in2="morph1" result="strokeText"/><feDisplacementMap xChannelSelector="R" yChannelSelector="G" in="strokeText" in2="noise" result="displacementMap" color-interpolation-filters="sRGB" scale="10" /> <feGaussianBlur stdDeviation="5" result="coloredBlur"/> <feMerge> <feMergeNode in="coloredBlur"/> <feMergeNode in="displacementMap"/> </feMerge></filter> <filter id="shock2"><feTurbulence type="fractalNoise" baseFrequency="0.2" numOctaves="1" seed="2" result="noise" id="noise"> <animate attributeType="XML" attributeName="seed" from="2" to="120" dur="10s" repeatCount="indefinite"/></feTurbulence><feMorphology id="morph3" in="SourceGraphic" operator="dilate" radius="1" result="morph1" /><feMorphology id="morph4" in="morph1" operator="dilate" radius="1" result="morph2" /><feComposite operator="out" in="morph2" in2="morph1" result="strokeText"/><feDisplacementMap xChannelSelector="R" yChannelSelector="G" in="strokeText" in2="noise" result="displacementMap" color-interpolation-filters="sRGB" scale="10" /> <feGaussianBlur stdDeviation="5" result="coloredBlur"/> <feMerge> <feMergeNode in="coloredBlur"/> <feMergeNode in="displacementMap"/> </feMerge></filter> <use xlink:href="#{TEXT}" id="textShock" x="2" y="2" filter="url(#shock)" stroke="0" stroke-width="1" fill="{TEXT2}"/> <use xlink:href="#{TEXT}" id="textShock" x="2" y="2" filter="url(#shock2)" stroke="0" stroke-width="1" fill="{TEXT3}"/> <use xlink:href="#{TEXT}" id="textFill" x="0" y="0" fill="{TEXT4}" /> <use xlink:href="#{TEXT}" id="textGlow" x="0" y="0" filter="url(#glow)" stroke="{TEXT3}" stroke-width="0.1" fill="transparent"/></svg>'
+            '[ELECTRIFY]{TEXT},{COLOR},{COLOR2},{COLOR3}[/ELECTRIFY]',
+'<svg xmlns:xlink="www.w3.org/1999/xlink" width="100%" height="100%"><defs><text id="{TEXT}" x="50%" y="50%" font-family="Voltaire" font-size="68px" font-weight="600" text-anchor="middle" alignment-baseline="middle">{TEXT}</text></defs><filter id="glow"> <feGaussianBlur id="glowBlur" stdDeviation="3.5" result="coloredBlur"/><feMerge><feMergeNode in="coloredBlur"/><feMergeNode in="SourceGraphic"/></feMerge></filter><filter id="shock"><feTurbulence type="fractalNoise" baseFrequency="0.2" numOctaves="1" seed="2" result="noise" id="noise"> <animate attributeType="XML" attributeName="seed" from="2" to="120" dur="12s" repeatCount="indefinite"/></feTurbulence><feMorphology id="morph1" in="SourceGraphic" operator="dilate" radius="10" result="morph1" /><feMorphology id="morph2" in="morph1" operator="dilate" radius="2" result="morph2" /><feComposite operator="out" in="morph2" in2="morph1" result="strokeText"/><feDisplacementMap xChannelSelector="R" yChannelSelector="G" in="strokeText" in2="noise" result="displacementMap" color-interpolation-filters="sRGB" scale="10" /> <feGaussianBlur stdDeviation="5" result="coloredBlur"/> <feMerge> <feMergeNode in="coloredBlur"/> <feMergeNode in="displacementMap"/> </feMerge></filter> <filter id="shock2"><feTurbulence type="fractalNoise" baseFrequency="0.2" numOctaves="1" seed="2" result="noise" id="noise"> <animate attributeType="XML" attributeName="seed" from="2" to="120" dur="10s" repeatCount="indefinite"/></feTurbulence><feMorphology id="morph3" in="SourceGraphic" operator="dilate" radius="1" result="morph1" /><feMorphology id="morph4" in="morph1" operator="dilate" radius="1" result="morph2" /><feComposite operator="out" in="morph2" in2="morph1" result="strokeText"/><feDisplacementMap xChannelSelector="R" yChannelSelector="G" in="strokeText" in2="noise" result="displacementMap" color-interpolation-filters="sRGB" scale="10" /> <feGaussianBlur stdDeviation="5" result="coloredBlur"/> <feMerge> <feMergeNode in="coloredBlur"/> <feMergeNode in="displacementMap"/> </feMerge></filter> <use xlink:href="#{TEXT}" id="textShock" x="2" y="2" filter="url(#shock)" stroke="0" stroke-width="1" fill="{COLOR}"/> <use xlink:href="#{TEXT}" id="textShock" x="2" y="2" filter="url(#shock2)" stroke="0" stroke-width="1" fill="{COLOR2}"/> <use xlink:href="#{TEXT}" id="textFill" x="0" y="0" fill="{COLOR3}" /> <use xlink:href="#{TEXT}" id="textGlow" x="0" y="0" filter="url(#glow)" stroke="{COLOR}" stroke-width="0.1" fill="transparent"/></svg>'
         );
 
 
 $event->configurator->BBCodes->addCustom(
             '[bposter]{COLOR},{COLOR2},{COLOR3},{COLOR4},{TEXT},{TEXT2},{TEXT3},{TEXT4},{TEXT5}[/bposter]',
             '<div id="posbody"><h1 class="posh1" style="--main-background: {COLOR}; --main-border: {COLOR2}; --main-strong: {COLOR3}; --main-span: {COLOR4}"><postrong>{TEXT}</postrong><pospan>{TEXT2}</pospan><postrong>{TEXT3}</postrong><pospan>{TEXT4}</pospan><postrong>{TEXT5}</postrong></h1></div>'
+        );
+
+
+$event->configurator->BBCodes->addCustom(
+            '[CALICON]{TEXT},{TEXT2},{NUMBER},{COLOR},{COLOR2},{COLOR3},{COLOR4},{COLOR5}[/CALICON]',
+            '<div id="calbody" style="--main-month-bg: {COLOR}; --main-month-font: {COLOR2}; --main-day-number-font: {COLOR3}; --main-cal-line: {COLOR4}; --main-cal-body: {COLOR5};">
+<time datetime="2014-09-20" class="icon">
+  <calem>{TEXT}</calem>
+  <calstrong>{TEXT2}</calstrong>
+  <calspan>{NUMBER}</calspan>
+</time>
+</div>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+            '[bgscroll][/bgscroll]',
+            '<div id="scrollbody" style="--main-scroll-font: red; --main-scroll-font-size: 30px; --main-scroll-bg: transparent;"><p class="scrolltext"> Example for Scrolling Pattern</p></div>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+            '[MMARK][/MMARK]',
+            '<div id="mpinbody"><div id="mpin"></div><div id="mpulse"></div></div>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+            '[TOGBOX]{COLOR},{COLOR2},{TEXT};{TEXT2};{TEXT3};{TEXT4};{TEXT5};{TEXT6};{TEXT7};{TEXT8};{TEXT9};{TEXT10}[/TOGBOX]',
+            '<div id="togbody" style="--main-togbox-bg: {COLOR}; --main-togbox-label: {COLOR2}"><div class="togbox"><div>
+    <input id="{TEXT}" type="radio" name="toggle" checked="checked"/>
+    <label for="{TEXT}">{TEXT}</label>
+    <div class="togcontent">
+      <p class="togp">{TEXT2}</p></div>
+  </div>
+  <div>
+    <input id="{TEXT3}" type="radio" name="toggle"/>
+    <label for="{TEXT3}">{TEXT3}</label>
+    <div class="togcontent">
+      <p class="togp">{TEXT4}</p></div>
+  </div>
+  <div>
+    <input id="{TEXT5}" type="radio" name="toggle"/>
+    <label for="{TEXT5}">{TEXT5}</label>
+    <div class="togcontent">
+      <p class="togp">{TEXT6}</p></div>
+  </div>
+  <div>
+    <input id="{TEXT7}" type="radio" name="toggle"/>
+    <label for="{TEXT7}">{TEXT7}</label>
+    <div class="togcontent">
+      <p class="togp">{TEXT8}</p></div>
+  </div>
+  <div>
+    <input id="{TEXT9}" type="radio" name="toggle"/>
+    <label for="{TEXT9}">{TEXT9}</label>
+    <div class="togcontent">
+      <p class="togp">{TEXT10}</p></div>
+  </div></div></div>'
         );
 
 
